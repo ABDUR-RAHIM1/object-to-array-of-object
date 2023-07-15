dff5fa4545240a58cb88f272fe3ad755f04b9cb3
AbdurRahim <89129980+ABDUR-RAHIM1@users.noreply.github.com>
2023-07-15 08:23:52 +0600

    Add files via upload

---
+++ Data/Data.jsx
@@ -0,0 +1,17 @@
+export const Data =  {
+        "name" : {
+            "label" : "your name",
+             "type " : "text",
+             "placeholder" : "Enter Your Name"
+        },
+        "email" : {
+            "label" : "your email",
+             "type " : "email",
+             "placeholder" : "Enter Your email"
+        },
+        "password" : {
+            "label" : "your password",
+             "type " : "text",
+             "placeholder" : "Enter Your password"
+        },
+     } 
